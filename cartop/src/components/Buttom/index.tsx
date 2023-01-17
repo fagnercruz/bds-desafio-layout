e8d591df3597d0a491f31f2e231c3876551ac975
Fagner Cruz <fagner@sti.ufpb.br>
2023-01-17 12:51:42 -0300

    finish home in 320px breakpoint

---
+++ cartop/src/components/Buttom/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Buttom = () => {
+    return (
+        <div className="btn btn-site">
+            VER CATÁLOGO
+        </div>
+    );
+
+};
+
+export default Buttom;
--- cartop/src/pages/Home/index.tsx
+++ cartop/src/pages/Home/index.tsx
@@ -1,10 +1,28 @@
 import './style.css';
+import ImgHeader from 'assets/images/car-header-1.png';
+import Buttom from 'components/Buttom';
 
 const Home = () => {
     return (
         <>
-            <div className="container">
-                <h4>Olá</h4>
+            <div>
+                <div className='base-card main-card'>
+                    <div className='main-card-contents'> 
+                        <h1>O carro perfeito para você</h1>
+                        <p>Conheça nossos carros e dê mais um 
+                            passo na realização do seu sonho</p>
+                    </div>
+
+                    <div className='main-card-image'>
+                        <img src={ImgHeader} alt="Carro amarelo" />
+                    </div>
+                </div>
+ 
+                <div className="base-card secondary-card">
+                    <Buttom />
+                    <p>Começe agora a navegar</p>
+                </div>
+
             </div>
         </>
     );
